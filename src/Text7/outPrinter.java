5c37a10d9707a2b8a1d6c9c1a0ca235468b85baa
GZH <gf2711456042@gmail.com>
2020-11-04 17:56:18 +0800

    第7章作业第一问

---
+++ src/Text7/outPrinter.java
@@ -0,0 +1,19 @@
+package Text7;
+
+import java.io.*;
+
+public class outPrinter {
+    public static void main(String[] args) {
+        File f1 = new File("E:/Java/workspace/src/Text7/1.txt");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f1));
+            String s;
+            while((s=br.readLine()) != null){
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
